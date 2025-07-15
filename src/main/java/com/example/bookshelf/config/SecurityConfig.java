29dd859f5cb5fa0fe1fbe8d4acf72a69941f3606
janemir <janemiro2005@gmail.com>
2025-07-15 13:31:23 +0300

    without: Failed to load remote configuration.

--- src/main/java/com/example/bookshelf/config/SecurityConfig.java
+++ src/main/java/com/example/bookshelf/config/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/v3/api-docs",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/api-docs/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
