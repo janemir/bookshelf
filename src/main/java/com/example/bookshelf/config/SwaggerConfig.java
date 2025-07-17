93db29d0436273b780a880e1e6270ee922479e7b
janemir <janemiro2005@gmail.com>
2025-07-17 12:41:15 +0300

    Настроена JWT авторизация: генерация токена при логине, добавление токена в Authorization header.

--- src/main/java/com/example/bookshelf/config/SwaggerConfig.java
+++ src/main/java/com/example/bookshelf/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.example.bookshelf.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +15,7 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI api(){
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
                 .servers(
                         List.of(
@@ -21,6 +24,17 @@ public class SwaggerConfig {
                 )
                 .info(
                        new Info().title("Bookshelf API")
+                )
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new io.swagger.v3.oas.models.Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
                 );
     }
 }
